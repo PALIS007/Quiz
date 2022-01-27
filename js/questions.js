a5a94714e40dad0ffffec21992e7fd63f86ff773
Harshit0007 <86467483+PALIS007@users.noreply.github.com>
2022-01-27 16:13:34 +0530

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,92 @@
+You're running out of storage (95%). … Soon you won't be able to upload new files to Drive and send or receive emails in Gmail.Learn more
+questions.js
+Who has access
+H
+
+System properties
+Type
+Javascript
+Size
+2 KB
+Storage used
+2 KB
+Location
+js
+Owner
+Harshit Paliwal
+Modified
+Jan 26, 2022 by Harshit Paliwal
+Opened
+4:12 PM by me
+Created
+4:04 PM
+Add a description
+Viewers can download
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+     numb: 6,
+    question: "What is Full form of DBMS",
+     answer: "Database Managment System",
+     options: [
+      "Database Managment System",
+      "Database Manage System",
+      "Data Management System",
+      "Database Managment "
+     ]
+   },
+];
